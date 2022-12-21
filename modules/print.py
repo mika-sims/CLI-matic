b16bb8a5f69dd4419f7ae1da56a6e8d8f9c8774b
Mikail Simsek <mikailsimsek.trb@gmail.com>
2022-12-21 18:58:55 +0100

    create greeting function to print greeting and call banner function on it

--- modules/print.py
+++ modules/print.py
@@ -6,3 +6,22 @@ import pyfiglet
 
 # Import colored modul from termcolor for colored output
 from termcolor import colored
+
+def banner():
+    """
+    Prints application's banner.
+    """
+
+    banner = pyfiglet.figlet_format("CLI - matic", justify="center")
+    print(colored(banner, "yellow"))
+
+def greeting():
+    """
+    Prints a welcome message to the screen
+    """
+    greeting = pyfiglet.figlet_format("WELCOME", justify="center")
+    print(colored(greeting, "green"))
+    banner()
+    print("Display weather forecasts for over 200,000 cities".center(80))
+    print(colored("Press Enter to start", "green").center(90))
+    input("".center(40))
