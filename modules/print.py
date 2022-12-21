2283409448c1d8108f5c2057b84ef18751c27179
Mikail Simsek <mikailsimsek.trb@gmail.com>
2022-12-21 18:40:26 +0100

    create print.py module and import libraries

---
+++ modules/print.py
@@ -0,0 +1,8 @@
+# Import os library
+import os
+
+# Import pyfiglet to create the app banner
+import pyfiglet
+
+# Import colored modul from termcolor for colored output
+from termcolor import colored
