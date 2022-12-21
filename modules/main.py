db4b4337a75c24023b413caa5cdfdcdbec94a826
Mikail Simsek <mikailsimsek.trb@gmail.com>
2022-12-21 23:18:42 +0100

    add function to set the URL for geolocation API call

--- modules/main.py
+++ modules/main.py
@@ -26,4 +26,21 @@ def get_target_location():
             break
     return target_location
 
-get_target_location()
+
+def set_geolocation_url(target_location):
+    """
+    Sets the URL of the API call to find the coordinates
+    of the target location
+
+    Args:
+        target_location (int): The return value of the
+                                get_target_location function 
+    """
+    
+    if target_location == 1:
+        geolocation_url = f"https://api.ipdata.co?api-key={IPDATA_API_KEY}"
+    elif target_location == 2:
+        geolocation_url = f"http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country}&limit=3&appid={OWM_API_KEY}"
+
+    return geolocation_url
+
